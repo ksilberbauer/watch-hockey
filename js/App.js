52eb650b437d995574fd5a205ff8fdfd1709f3e8
Kyle Silberbauer <kyle.silberbauer@outlook.com>
2015-11-12 19:29:38 -0500

    +updated styles

--- js/App.js
+++ js/App.js
@@ -19,26 +19,40 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
-        <label>
-          URL: 
-          <input ref={ ref => this.urlInput = ref } defaultValue={this.props.defaultUrl} />
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around', height: '100%' }}>
+        <label style={{  flexGrow: 1, display: 'flex', alignItems: 'center' }}>
+          <span style={{ flexGrow: 1 }}>URL:</span>
+          <input 
+            ref={ ref => this.urlInput = ref }
+            style={{ flexGrow: 2 }}
+            defaultValue={this.props.defaultUrl} />
         </label>
-        <button onClick={this.start}>Start</button>
-        <button onClick={ () => sendMessage(this.props.stopAction()) }>
-          Stop
-        </button>
-        <button onClick={ () => sendMessage(this.props.clearParams()) }>
-          Clear Params
-        </button>
-        <label>
+        <label style={{ flexGrow: 2, display: 'flex', alignItems: 'center' }}>
           Refocus Opener: 
           <input
             type='checkbox'
             onChange={ e => this.doRefocus(e.target.checked) } 
-            checked={ this.state.doRefocusChecked } />
+            checked={ this.state.doRefocusChecked }
+            style={styles.clickable} />
         </label>
+        <div style={{ flexGrow: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <button onClick={this.start} style={styles.clickable}>Start</button>
+          <button onClick={ () => sendMessage(this.props.stopAction()) } style={styles.clickable}>
+            Stop
+          </button>
+        </div>
+        <div style={{ flexGrow: 2, display: 'flex', alignItems: 'center' }}>
+          <button onClick={ () => sendMessage(this.props.clearParams()) } style={styles.clickable}>
+            Clear Params
+          </button>
+        </div>
       </div>
     );
   }
 }
+
+const styles = {
+  clickable: {
+    cursor: 'pointer',
+  }
+};
